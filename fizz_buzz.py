958c0b0bb113c96ba0c39c35de42d7b2cd9b7aa9
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-08-16 12:39:10 +0100

    implemented number 412

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,4 +3,10 @@ from typing import List
 
 
 def fizz_buzz(n: int) -> List[str]:
-    return ['']
+    r = []
+    for i in range(1, n+1):
+        s = 'Fizz' if i % 3 == 0 else ''
+        s += 'Buzz' if i % 5 == 0 else ''
+        r.append(s) if s else r.append(str(i))
+
+    return r
