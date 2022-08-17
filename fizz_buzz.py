0e5d67fdf8aec7497452259733429b2296417e99
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-08-17 07:58:39 +0100

    implemented number 412 one line

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -5,8 +5,6 @@ from typing import List
 def fizz_buzz(n: int) -> List[str]:
     r = []
     for i in range(1, n+1):
-        s = 'Fizz' if i % 3 == 0 else ''
-        s += 'Buzz' if i % 5 == 0 else ''
-        r.append(s) if s else r.append(str(i))
+        r.append('Fizz'*(i % 3 < 1)+'Buzz'*(i % 5 < 1) or str(i))
 
     return r
